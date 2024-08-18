8954375810711b31d7668aa54f4b76b0f5ec2aaa
Jonathan Rozario <jonathanrozario@gmail.com>
2024-08-18 12:11:08 -0400

    chore: Open edit window before trigger

--- cpr.sh
+++ cpr.sh
@@ -1,19 +1,20 @@
 # Create PR
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 IFS='-' read -r KIND PROJECT NUM REST <<< "$BRANCH"
 DESCRIPTION=$(echo $REST | tr '-' ' ')
-
 TICKET=$(echo "$PROJECT-$NUM")
 
 TITLE="$KIND: $TICKET $DESCRIPTION"
 
+AUTHOR='jnthnrzr'
+
 hub pull-request \
-    --assign jnthnrzr \
+    --assign "$AUTHOR" \
     --browse \
     --draft \
+    --edit \
     -m "$TITLE" \
     -m "Jira: https://sesamecare.atlassian.net/browse/$TICKET" \
     -m "### Description" \
     -m "" \
-    -m "$*"
