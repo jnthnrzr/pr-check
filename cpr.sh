5dc5edb301fa2c85b5a183acd09a7b2a38aeeef4
Jonathan Rozario <jonathanrozario@gmail.com>
2024-08-17 20:45:43 -0400

    chore: Add script to create PR

---
+++ cpr.sh
@@ -0,0 +1,9 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+IFS='-' read -r KIND PROJECT NUM REST <<< "$BRANCH"
+DESCRIPTION=$(echo $REST | tr '-' ' ')
+
+TICKET=$(echo "$PROJECT-$NUM")
+echo $KIND: $TICKET $DESCRIPTION
+
+hub pull-request --assign jnthnrzr --browse --draft -m -F --
